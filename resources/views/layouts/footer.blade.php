8bd97086caa7bf3df011fab1a3307206ff6f168d
Nicas Lolela <lolelanicas@gmail.com>
2024-09-17 18:17:38 +0300

    Commited Updates.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -83,6 +83,19 @@
   
       </div>
     </div>
+    <script>
+//Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66d5d0a150c10f7a00a31c18/1i6pjdudj';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+//End of Tawk.to Script
+    </script>
   
   </footer>
   
